5a44a171153ca450942c5ccbfcf05fc922882a4d
Manuel Felipe <manuel123.mfv@gmail.com>
2025-10-11 14:36:41 -0500

    Cambios en frontend y logica

--- script.js
+++ script.js
@@ -1,208 +1,57 @@
-//Opciones
-const productosAg = document.getElementById("productosAgregados");
-const total = document.getElementById("pagoTotal");
-const botones = document.getElementById("botones");
-const borrar = document.getElementById("borrar");
-let suma = [];
-let historial = [];
+const botonAg = document.getElementById('botonAg');
+const botonCa = document.getElementById('botonCa');
+const productos = document.getElementById('productos');
+const cantidad = document.getElementById('cantidad');
+const productosAg = document.getElementById('productosAg');
+const totalFinal = document.getElementById('totalFinal');
+const producto = {
+    lomo: 10000,
+    espaldilla: 9000,
+    bola: 10000,
+    cadera: 9000,
+    pepino: 9000,
+    hueso: 5000,
+    costilla: 9000,
+    espinazo: 5000,
+    molida: 9000
+};
+// Array para guardar los productos agregados
+const listaProductos = [];
 
-// Productos
-const lomo = document.getElementById("lomo");
-const espaldilla = document.getElementById("espaldilla");
-const bola = document.getElementById("bola");
-const cadera = document.getElementById("cadera");
-const pepino = document.getElementById("pepino");
-const hueso = document.getElementById("hueso");
-const costilla = document.getElementById("costilla");
-const espinazo = document.getElementById("espinazo");
-const molida = document.getElementById("molida");
+botonAg.addEventListener('click', () => {
+  const prodSeleccionado = productos.value;
+  const cant = Number(cantidad.value);
 
-// Contadores
-let contadorLomo = 0;
-let contadorEspaldilla = 0;
-let contadorBola = 0;
-let contadorCadera = 0;
-let contadorPepino = 0;
-let contadorHueso = 0;
-let contadorCostilla = 0;
-let contadorEspinazo = 0;
-let contadorMolida = 0;
-
-//Boton borrar
-borrar.addEventListener("click", () => {
-  if (historial.length > 0) {
-    let ultimo = historial.pop();
-    suma.pop();
-
-    if (ultimo === "espaldilla") {
-      contadorEspaldilla--;
-      document.getElementById("espaldillaItem").innerText = 
-        contadorEspaldilla > 0 ? `Espaldilla - ${contadorEspaldilla}` : "";
-    }
-    if (ultimo === "lomo"){
-        contadorLomo--;
-        document.getElementById("lomoItem").innerText =
-        contadorLomo > 0 ? `Lomo - ${contadorLomo}` : "";
-    }
-    if (ultimo === "bola"){
-        contadorBola--;
-        document.getElementById("bolaItem").innerText=
-        contadorBola > 0 ? `Bola - ${contadorBola}` : "";
-    }
-    if (ultimo === "cadera"){
-        contadorCadera--;
-        document.getElementById("caderaItem").innerText = 
-        contadorCadera > 0 ? `Cadera - ${contadorCadera}` : "";
-    }
-    if (ultimo === "pepino"){
-        contadorPepino--;
-        document.getElementById("pepinoItem").innerText = 
-        contadorPepino > 0 ? `Pepino - ${contadorPepino}` : "";
-    }
-    if (ultimo === "hueso"){
-        contadorHueso--;
-        document.getElementById("huesoItem").innerText = 
-        contadorHueso > 0 ? `Hueso - ${contadorHueso}`: "";
-    }
-    if (ultimo === "costilla"){
-        contadorCostilla--;
-        document.getElementById("costillaItem").innerText = 
-        contadorCostilla > 0 ? `Costilla - ${contadorCostilla}`: "";
-    }
-    if (ultimo === "espinazo"){
-        contadorEspinazo--;
-        document.getElementById("espinazoItem").innerText = 
-        contadorEspinazo > 0 ? `Espinazo - ${contadorEspinazo}`: "";
-    }
-    if (ultimo === "molida"){
-        contadorMolida--;
-        document.getElementById("molidaItem").innerText = 
-        contadorMolida > 0 ? `Carne molida - ${contadorMolida}` : "";
-    }
+  if (!prodSeleccionado || !cant) {
+    alert('Selecciona un producto y una cantidad válida');
+    return;
   }
+
+  const precio = producto[prodSeleccionado];
+  const total = precio * cant;
+  
+  // Guardar en el arreglo
+    listaProductos.push(total);
+
+  // Agregamos el producto a la lista
+  const item = document.createElement('p');
+  item.classList.add('productoag')
+  item.textContent = `${prodSeleccionado} (${cant} lb): $${total.toLocaleString()}`;
+  productosAg.appendChild(item);
+
+  // Limpia el input cantidad
+  cantidad.value = '';
 });
 
-
-//Boton resultado final
-botones.addEventListener('click',() =>{
-    let valores = suma.reduce((acumulado,valorActual)=>acumulado + valorActual,0);
-    total.innerHTML = `Total: $${valores.toLocaleString("es-ES")}`
-});
- 
-//Botones productos
-lomo.addEventListener('click', ()=>{
-    let p = document.getElementById("lomoItem");
-    contadorLomo++;
-    if(!p){
-        p = document.createElement("p");
-        p.id = "lomoItem";
-        productosAg.appendChild(p);
+botonCa.addEventListener('click', () => {
+    if (listaProductos.length === 0) {
+        alert('No hay productos agregados');
+        return;
     }
-    p.textContent = `Lomo - ${contadorLomo}` 
-    suma.push(10000)
-    historial.push("lomo")
-});
 
-espaldilla.addEventListener('click',()=>{
-    let p = document.getElementById("espaldillaItem");
-    contadorEspaldilla++;
-    if(!p){
-        p = document.createElement("p")
-        p.id = "espaldillaItem";
-        productosAg.appendChild(p);
-    }
-    p.textContent = `Espaldilla - ${contadorEspaldilla}`;
-    suma.push(9000);
-    historial.push("espaldilla");
-});
+    // Sumar todos los totales
+    const totalFinalVenta = listaProductos.reduce((acc, val) => acc + val, 0);
 
-bola.addEventListener('click', ()=>{
-   let p = document.getElementById("bolaItem");
-   contadorBola++;
-   if(!p){
-    p = document.createElement("p");
-    p.id = "bolaItem";
-    productosAg.appendChild(p);
-   }
-   p.textContent = `Bola - ${contadorBola}`;
-    suma.push(10000);
-    historial.push("bola");
-});
-
-cadera.addEventListener('click', ()=>{
-    let p = document.getElementById("caderaItem");
-    contadorCadera++
-    if(!p){
-        p = document.createElement("p");
-        p.id = "caderaItem"
-        productosAg.appendChild(p)
-    }
-    p.textContent = `Cadera - ${contadorCadera}`;
-    suma.push(9000)
-    historial.push("cadera");
-});
-
-pepino.addEventListener('click', ()=>{
-    let p = document.getElementById("pepinoItem");
-    contadorPepino++;
-    if(!p){
-        p = document.createElement("p");
-        p.id = "pepinoItem"
-        productosAg.appendChild(p)
-    }
-    p.textContent = `Pepino - ${contadorPepino}`;
-    suma.push(9000);
-    historial.push("pepino");
-});
-
-hueso.addEventListener('click', ()=>{
-    let p = document.getElementById("huesoItem");
-    contadorHueso++;
-    if(!p){
-        p = document.createElement("p");
-        p.id = "huesoItem";
-        productosAg.appendChild(p);
-    }
-    p.textContent = `Hueso - ${contadorHueso}`;
-    suma.push(5000);
-    historial.push("hueso");
-});
-
-costilla.addEventListener('click', ()=>{
-    let p = document.getElementById("costillaItem");
-    contadorCostilla++;
-    if(!p){
-        p = document.createElement("p");
-        p.id = "costillaItem";
-        productosAg.appendChild(p);
-    }
-    p.textContent = `Costilla - ${contadorCostilla}`;
-    suma.push(9000);
-    historial.push("costilla");
-});
-
-espinazo.addEventListener('click', ()=>{
-    let p = document.getElementById("espinazoItem");
-    contadorEspinazo++;
-    if (!p){
-        p = document.createElement("p");
-        p.id = "espinazoItem";
-        productosAg.appendChild(p);
-    }
-    p.textContent = `Espinazo - ${contadorEspinazo}`;
-    suma.push(5000);
-    historial.push("espinazo");
-});
-
-molida.addEventListener('click', ()=>{
-    let p = document.getElementById("molidaItem");
-    contadorMolida++;
-    if(!p){
-        p = document.createElement("p");
-        p.id = "molidaItem";
-        productosAg.appendChild(p);
-    }
-    p.textContent = `Carne molida - ${contadorMolida}`;
-    suma.push(9000);
-    historial.push("molida")
+    // Mostrar total en pantalla
+    totalFinal.textContent = `$${totalFinalVenta.toLocaleString()}`;
 });
